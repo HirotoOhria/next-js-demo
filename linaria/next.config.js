de265310fb65b3ab0740113a9d5b2471743375b5
hiroto.ohira <hiroto100114@gmail.com>
2022-04-24 13:49:09 +0900

    Update next.config.js to use next-linaria.

--- linaria/next.config.js
+++ linaria/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withLinaria = require('next-linaria')
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const linariaOption = {}
+
+module.exports = withLinaria({
+  ...nextConfig,
+  linaria: linariaOption,
+})
