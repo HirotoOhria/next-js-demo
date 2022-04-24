a4d5ee7cfc72b6b7dad9702be0079ef1cb264fa1
hiroto.ohira <hiroto100114@gmail.com>
2022-04-24 16:34:56 +0900

    Refactor to use yarn.

--- linaria/pages/_app.tsx
+++ linaria/pages/_app.tsx
@@ -1,19 +1,8 @@
-import { AppProps } from 'next/app'
-import Head from 'next/head'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-const App = ({ Component, pageProps }: AppProps) => (
-    <>
-        <Head>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <title>Linaria demo on Next.js</title>
-            {/* <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" /> */}
-            {/* <link rel="manifest" href="/manifest.json" /> */}
-        </Head>
-        <Component {...pageProps} />
-    </>
-)
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
 
-export default App
+export default MyApp
